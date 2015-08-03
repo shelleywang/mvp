13e15f41d0182abe34c5ab1082b47db4b6f94fc2
Shelley Wang <shelleyw13@gmail.com>
2015-08-03 16:13:56 -0700

    Add basic styling

--- client/app/app.js
+++ client/app/app.js
@@ -2,6 +2,12 @@ angular.module('hackerHelpr',
   ['Answers',
    'Search',
    'ui.router'])
+.directive('header', function() {
+  return {
+    restrict: 'E',
+    template: '<h1><a class="header" href="#/search">HackerHelpr</a></h1>'
+  };
+})
 .config(function($stateProvider,$urlRouterProvider) {
   $urlRouterProvider.otherwise('/search');
 
