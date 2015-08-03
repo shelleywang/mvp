e82753c3f4958a5efbc1b43aead137d013b56676
Shelley Wang <shelleyw13@gmail.com>
2015-08-03 15:38:49 -0700

    Refactor to use ui-router

--- client/app/app.js
+++ client/app/app.js
@@ -1,18 +1,25 @@
 angular.module('hackerHelpr', 
   ['Answers',
    'Search',
-   'ngRoute'])
-.config(function($routeProvider) {
-  $routeProvider
-  .when('/search', {
+   'ui.router'])
+.config(function($stateProvider,$urlRouterProvider) {
+  $urlRouterProvider.otherwise('/search');
+
+  $stateProvider
+  .state('/search', {
+    url: '/search',
     templateUrl: 'app/search/search.html',
     controller: 'SearchCtrl'
   })
-  .when('/results', {
-    templateUrl: 'app/answers/answers.html',
-    controller: 'AnswersCtrl'
-  })
-  .otherwise({
-    redirectTo: '/search'
+  .state('/results', {
+    url: '/results',
+    views: {
+      'searchBar' : {
+        templateUrl: 'app/search/search.html',
+        controller: 'SearchCtrl'},
+      'resultsDiv' : {
+        templateUrl: 'app/answers/answers.html',
+        controller: 'AnswersCtrl'}
+    }
   })
 })
