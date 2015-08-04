0a2d83a2daea3f47efb2a1851b86c746a3208bc1
Shelley Wang <shelleyw13@gmail.com>
2015-08-03 19:15:54 -0700

    Add basic filtering for results, use /api/search for results page data, /api/search uses dummy data

--- client/app/answers/answers.js
+++ client/app/answers/answers.js
@@ -1,13 +1,31 @@
 var answers = angular.module('Answers', []);
 
-answers.controller('AnswersCtrl', function($scope, $http) {
-  $scope.test = {};
+answers.controller('AnswersCtrl', function($scope, $http, $filter) {
+  $scope.results = [];
+
+  $scope.orderingBy = 'views';
 
   $scope.populate = function() {
-    $http.get('test/sample.json').success(function(data) {
-      $scope.test = data.items;
+    // $http.get('test/sample.json').success(function(data) {
+    //   $scope.test = data.items;
+    // })
+    $http({
+      method: 'GET',
+      url: '/api/search',
     })
+    .then(function (resp) {
+      $scope.results = resp.data;
+    });
   };
 
   $scope.populate();
+
+  var orderBy = $filter('orderBy');
+
+  $scope.order = function(predicate, reverse) {
+    $scope.results = $filter('orderBy')($scope.results, predicate, true);
+    $scope.orderingBy = predicate;
+  };
+
+
 });
--- server/searchProcessor.js
+++ server/searchProcessor.js
@@ -7,12 +7,18 @@ var retrieveAnswers = function(query) {
 
   var filepath = __dirname+'/../client/test/sample.json';
   var file = fs.readFileSync(filepath, 'utf8');
-  return JSON.parse(file);
+  return JSON.parse(file).items;
 }
 
-var getStats = function(fulldata) {
+var removeUnanswered = function(data) {
+  var bestResults = data.filter(function (item) {
+    return item.answer_count >=1; 
+  })
+  return bestResults;
+}
+
+var getStats = function(data) {
   var results = [];
-  var data= fulldata.items;
   data.forEach(function(question) {
     var stats = {};
     stats.questionText = question.title;
@@ -21,6 +27,7 @@ var getStats = function(fulldata) {
     stats.views = question.view_count;
     stats.answerCount = question.answer_count;
     stats.postDate = question.creation_date;
+    stats.link = question.link;
     results.push(stats);
   });
 
@@ -31,9 +38,8 @@ var getStats = function(fulldata) {
 module.exports = {
 
   processSearch: function(req, res) {
-    var answers = retrieveAnswers(req.query);
-    console.log(getStats(answers));
-    res.status(200).send('Hello World');
+    var answers = removeUnanswered(retrieveAnswers(req.query));
+    res.status(200).send(getStats(answers));
   }
 
 };
